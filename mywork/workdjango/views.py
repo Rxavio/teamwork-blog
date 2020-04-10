ab34e6da3868ebdd7d69667f1533d46db1d64b0a
xavioo <rucahoxavi@gmail.com>
2020-04-10 10:18:55 +0200

    update article

--- mywork/workdjango/views.py
+++ mywork/workdjango/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render,redirect
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import (
     ListView,
     CreateView,
     DetailView,
+    UpdateView,
+    DeleteView
    
 )
 from .models import Post
@@ -17,7 +19,7 @@ class PostListView(ListView):
 
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
-    success_url = '/'
+    # success_url = '/'
     fields = ['title', 'content']
 
     def form_valid(self, form):
@@ -26,3 +28,18 @@ class PostCreateView(LoginRequiredMixin, CreateView):
 
 class PostDetailView(DetailView):
     model = Post     
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form) 
+       
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False     
