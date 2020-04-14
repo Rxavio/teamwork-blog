8755d1f06bbb56078887e791d117d9065f806324
xavioo <rucahoxavi@gmail.com>
2020-04-14 10:08:58 +0200

    add some admin customization and new fields on post model

--- mywork/workdjango/admin.py
+++ mywork/workdjango/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display=('title','date_posted','date_updated','author','status')
+    list_filter=('status','date_posted','date_updated')
+    search_fields=('title',)
+    list_editable=('status',)
+
+
+admin.site.register(Post,PostAdmin)
--- mywork/workdjango/models.py
+++ mywork/workdjango/models.py
@@ -6,10 +6,16 @@ from django.urls import reverse
 
 
 class Post(models.Model):
+    STATUS_CHOICES={
+     ('draft','Draft'),   
+     ('published','Published'), 
+    }
     title = models.CharField(max_length=45)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
+    date_updated = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    status =models.CharField(max_length=10,choices=STATUS_CHOICES,default='draft')
 
 
     def __str__(self):
